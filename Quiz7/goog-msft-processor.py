061840faf9826e8786a8008ff5ccb5c4dbbeb3e7
SeixasAldrich <78521803+SeixasAldrich@users.noreply.github.com>
2023-11-01 23:10:57 -0400

    Update goog-msft-processor.py

--- Quiz7/goog-msft-processor.py
+++ Quiz7/goog-msft-processor.py
@@ -12,6 +12,8 @@ if __name__ == "__main__":
 
     rawPrices = ssc.socketTextStream(sys.argv[1], int(sys.argv[2]))
 
+    date = rawPrices.map(lambda l: float(l.split(" ")[0]))
+
     googPrice = rawPrices.map(lambda l: float(l.split(" ")[1]))
     # msftPrice = rawPrices.map(lambda l: float(l.split(" ")[2]))
 
@@ -38,9 +40,20 @@ if __name__ == "__main__":
                          # .map(lambda price: (price, 1))\
                          # .reduce(lambda x, y: (x + y) / 2.0)\
                          # .map(lambda m: "40 day average: " + str(m))
+
+    bOrsYestgoog = bOrs = goog10Day.window(2,1)[1].join(goog40Day.window(2,1)[1]).reduce(lambda x, y: 1 if x > y else 0)
+
+
+    bOrsgoog = goog10Day.window(2,1)[0].join(goog40Day.window(2,1)[0]).reduce(lambda x, y: 1 if x > y else 0)
+
+    if (bOrsgoog == 1 && bOrsYestgoog == 0):
+        print(date + "buy" + goog)
+    elif (bOrsgoog == 0 && bOrsYestgoog == 1):
+        print(date + "sell" + goog)
+        
  
-    goog10Day.pprint()
-    goog40Day.pprint()
+    # goog10Day.pprint()
+    # goog40Day.pprint()
     # msftPrice.pprint()
 
     ssc.start()
