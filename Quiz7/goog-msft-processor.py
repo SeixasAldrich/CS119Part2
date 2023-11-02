5f8c32ea893421989251ba95f496a52f179a08b7
SeixasAldrich <78521803+SeixasAldrich@users.noreply.github.com>
2023-11-01 22:08:11 -0400

    Update goog-msft-processor.py

--- Quiz7/goog-msft-processor.py
+++ Quiz7/goog-msft-processor.py
@@ -12,8 +12,10 @@ if __name__ == "__main__":
 
     rawPrices = ssc.socketTextStream(sys.argv[1], int(sys.argv[2]))
 
-    googPrice = rawPrices.map(lambda l: (l.split(" ")(0), l.split(" ")(1))).collect()
-    msftPrice = rawPrices.map(lambda l: (l.split(" ")(0), l.split(" ")(2))).collect()
+    googPrice = rawPrices.map(lambda l: (l.split(" ")(0), l.split(" ")(1)))
+                         .foldLeft("")(lambda x, y: (x+" "+y))
+    msftPrice = rawPrices.map(lambda l: (l.split(" ")(0), l.split(" ")(2)))
+                         .foldLeft("")(lambda x, y: (x+" "+y))
  
     googPrice.pprint()
     msftPrice.pprint()
