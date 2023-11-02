2aba92188186b506ae7787029aa7a7b60b05ddca
SeixasAldrich <78521803+SeixasAldrich@users.noreply.github.com>
2023-11-01 22:47:40 -0400

    Update goog-msft-processor.py

--- Quiz7/goog-msft-processor.py
+++ Quiz7/goog-msft-processor.py
@@ -13,10 +13,31 @@ if __name__ == "__main__":
     rawPrices = ssc.socketTextStream(sys.argv[1], int(sys.argv[2]))
 
     googPrice = rawPrices.map(lambda l: (l.split(" ")[1]))
-    msftPrice = rawPrices.map(lambda l: (l.split(" ")[2]))
+    # msftPrice = rawPrices.map(lambda l: (l.split(" ")[2]))
+
+    goog10Day = googPrice.window(10,1)\
+                         # .map(lambda price: (price, 1))\
+                         .reduce(lambda x, y: mean(x, y))\
+                         # .filter()
+                         .map(lambda m: "10 day average: " + tr(m))
+    goog40Day = googPrice.window(40,1)\
+                         # .map(lambda price: (price, 1))\
+                         .reduce(lambda x, y: mean(x, y))\
+                         .map(lambda m: "40 day average: " + tr(m))
+
+    # msft10Day = msftPrice.window(10,1)\
+                         # .map(lambda price: (price, 1))\
+                         # .reduce(lambda x, y: mean(x, y))\
+                         # .filter()
+                         # .map(lambda m: "10 day average: " + tr(m))
+    # msft40Day = msftPrice.window(40,1)\
+                         # .map(lambda price: (price, 1))\
+                         # .reduce(lambda x, y: mean(x, y))\
+                         # .map(lambda m: "40 day average: " + tr(m))
  
-    googPrice.pprint()
-    msftPrice.pprint()
+    goog10Day.pprint()
+    goog40Day.pprint()
+    # msftPrice.pprint()
 
     ssc.start()
     ssc.awaitTermination()
